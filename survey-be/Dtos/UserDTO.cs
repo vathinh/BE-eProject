648160aeed5dd5fe48884a61d0be0b4449e94c64
sonnguyenGIT <hoason2428@gmail.com>
2023-05-12 19:27:09 +0700

    fix register login

--- survey-be/Dtos/UserDTO.cs
+++ survey-be/Dtos/UserDTO.cs
@@ -4,10 +4,10 @@
     {
         public required string UserName { get; set; }
         public required string Password { get; set; }
-        public required string RollNo { get; set; }
-        public required string UserClass { get; set; }
-        public required string Specification { get; set; }
-        public required string Section { get; set; }
+        //public required string RollNo { get; set; }
+        //public required string UserClass { get; set; }
+        //public required string Specification { get; set; }
+        //public required string Section { get; set; }
 
     }
 }
